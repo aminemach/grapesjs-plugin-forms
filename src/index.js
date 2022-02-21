117b560cdaa0e20e32d7a4e579371a5e0a0e8172
Amine Machfer <74466395+aminemach@users.noreply.github.com>
2022-02-21 10:35:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import loadComponents from './components';
 export default (editor, opts = {}) => {
 
   const config = {
-    blocks: ['form', 'input', 'textarea', 'select', 'button', 'label', 'checkbox', 'radio'],
+    blocks: ['form', 'input', 'textarea', 'select', 'button', 'label', 'checkbox', 'radio','carousel'],
     ...opts
   };
 
